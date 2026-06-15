4369db3939fe9725f7ac59501cec2bb34bab7097
Veronica Langella <verolangi00@gmail.com>
2026-06-15 17:58:44 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Esercizio
+Descrivi le specifiche tecniche di un elettrodomestico
+
+# Descrizione
+- dimensioni
+- capacità
+- alimentazione
+- modalità di cottura
+- efficienza energetica 
---
+++ assets/main.js
@@ -0,0 +1,15 @@
+console.log("script caricato");
+
+const oven = {
+    dimensioni: {
+        larghezza: 60,
+        altezza: 60,
+    },
+    capacita: 75,
+    alimentazione: "elettrico",
+    modalitaCottura: ["statico", "ventilato", "vapore"],
+    efficienzaEnergetica: "A"
+
+};
+
+console.log(oven);
